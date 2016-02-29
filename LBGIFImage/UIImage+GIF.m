52360bd504bc70c3b1f9619a2a3f3aebab93fdc8
Pocket7878 <poketo7878@gmail.com>
2016-02-29 16:38:12 +0900

    Simply Check scale and try to load 3x image and fallback to normal scale image.

--- LBGIFImage/UIImage+GIF.m
+++ LBGIFImage/UIImage+GIF.m
@@ -46,7 +46,25 @@
 +(UIImage*)animatedGIFNamed:(NSString *)name {
     CGFloat scale = [UIScreen mainScreen].scale;
     
-    if (scale > 1.0f) {
+    if (scale > 2.0f) {
+        NSString* retinaPath = [[NSBundle mainBundle] pathForResource:[name stringByAppendingString:@"@3x"] ofType:@"gif"];
+        
+        NSData* data = [NSData dataWithContentsOfFile:retinaPath];
+        
+        if (data) {
+            return [UIImage animatedGIFWithData:data];
+        }
+
+        NSString* path = [[NSBundle mainBundle] pathForResource:name ofType:@"gif"];
+        
+        data = [NSData dataWithContentsOfFile:path];
+        
+        if (data) {
+            return [UIImage animatedGIFWithData:data];
+        }
+        
+        return [UIImage imageNamed:name];
+    } else if (scale > 1.0f) {
         NSString* retinaPath = [[NSBundle mainBundle] pathForResource:[name stringByAppendingString:@"@2x"] ofType:@"gif"];
         
         NSData* data = [NSData dataWithContentsOfFile:retinaPath];
@@ -64,8 +82,7 @@
         }
         
         return [UIImage imageNamed:name];
-    }
-    else {
+    } else {
         NSString* path = [[NSBundle mainBundle] pathForResource:name ofType:@"gif"];
         
         NSData* data = [NSData dataWithContentsOfFile:path];
